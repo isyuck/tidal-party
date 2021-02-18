5cee382dfe8f3300b70e3dd715f26acf482ac951
isyuck <isaac@isaac.ac>
2021-02-18 13:07:55 +0000

    a config file for configuring configurable things

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    twitch: {
+        identity: {
+            username: "YOUR_BOT_ACCOUNT",
+            password: "YOUR_TWITCH_OAUTH",
+        },
+        channels: [
+            "YOUR_CHANNEL_NAME"
+        ],
+    },
+    network: {
+        host: "127.0.0.1",
+        address: "/ctrl",
+        port: 6060,
+    },
+    maxActivePatterns: 8,
+};
+
+module.exports = config;
