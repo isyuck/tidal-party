7d61b78ee0aac0701122fca7c13bac3cf71da0c0
isaac <isaac@isaac.ac>
2021-04-16 18:28:39 +0100

    rework to use algo list to modify pattern list

--- pattern-handler.js
+++ pattern-handler.js
@@ -1,3 +1,32 @@
+// an array of functions that take a new pattern & user pair, a
+// list of all current patterns & user pairs, and the max active
+// patterns, and returns a new list of pattern & user pairs
+const algorithm = [
+    // algorithm[0] // simple user stack. add patterns until
+    // maxlen is reached, then replace the oldest pattern
+    // with the newest.
+    function(latest, all, maxlen) {
+        if (!all.length) {
+            all.push(latest);
+        } else if (all.length < maxlen) {
+            let match = false;
+            for (let p of all) {
+                if (p.user === latest.user) {
+                    Object.assign(p, latest);
+                    match = true;
+                    break;
+                }
+            }
+            if (!match) { all.push(latest) }
+        } else if (all.length === maxlen) {
+            all.push(current);
+            all.shift();
+        }
+        return all;
+    },
+    // add new algorithms here
+];
+
 module.exports = class PatternHandler {
     constructor(mode, maxActivePatterns) {
         this.mode = mode;   // decides pattern update algorithm
@@ -5,31 +34,9 @@ module.exports = class PatternHandler {
         this.maxActivePatterns = maxActivePatterns;
     }
 
-    // pass a pattern & user pair, and get a new list of pattern
-    // & user pairs. uses 'mode' to decide which algorithm to use
-    update(current) {
-        const len = this.patterns.length;
-        if (this.mode == 0) {
-            if (!len) {
-                this.patterns.push(current);
-            } else if (len < this.maxActivePatterns) {
-                let match = false;
-                for (let pattern of this.patterns) {
-                    if (pattern.user === current.user) {
-                        Object.assign(pattern, current);
-                        match = true;
-                        break;
-                    }
-                }
-                if (!match) { this.patterns.push(current) }
-            } else if (len === config.maxActivePatterns) {
-                this.patterns.push(current);
-                this.patterns.shift();
-            }
-            return this.patterns;
-        }
-
-        // no mode match
+    // pass a pattern & user pair, and get a new list of pattern & user pairs
+    update(latest) {
+        this.patterns = algorithm[this.mode](latest, this.patterns, this.maxActivePatterns);
         return this.patterns;
     }
 }
