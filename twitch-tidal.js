ce70c2cdca602408ebf2a57a4f9c23d1ec1ab166
isaac <isaac@isaac.ac>
2021-04-16 16:25:53 +0100

    add multiuser support, remove osc code

--- twitch-tidal.js
+++ twitch-tidal.js
@@ -1,9 +1,5 @@
 const config = require("./config.js");
 // const ui = require("./ui.js");
-const effectsList = require("./effects.json");
-const osc = require("node-osc");
-const oscClient = new osc.Client(config.network.host, config.network.port);
-const oscAddr = config.network.address;
 const tmi = require("tmi.js");
 const { spawn } = require('child_process');
 
@@ -17,7 +13,7 @@ twitchClient.connect();
 
 // tidal
 const tidal = spawn('ghci', ['-ghci-script', config.ghci.path]);
-tidal.stdout.on('data', (data) => { console.log(`tidal: ${String(data).trim()}`) });
+tidal.stdout.on('data', (data) => { console.log(`tidal ${String(data).trim()}`) });
 
 tidal.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`);
@@ -25,17 +21,42 @@ tidal.stderr.on('data', (data) => {
 
 // ui.render();
 
-function updateUI() {
-  for ([i, connection] of connections.entries()) {
-    ui.addConnection(i, connection)
-  }
-}
+// function updateUI() {
+//   for ([i, connection] of connections.entries()) {
+//     ui.addConnection(i, connection)
+//   }
+// }
 
 function handleNewMessage(msg, username) {
-    // updateUI()
-  msg = msg.substr(msg.indexOf(" ") + 1)
-  console.log(msg, username);
-  tidal.stdin.write(msg + "\n");
+  // updateUI()
+  msg = msg.substr(msg.indexOf(" ") + 1);
+
+  let current = ({ user: username, pattern: msg });
+
+  if (!connections.length) {
+    connections.push(current);
+  } else if (connections.length < config.maxActivePatterns) {
+    let match = false;
+    for (connection of connections) {
+      if (connection.user === current.user) {
+        Object.assign(connection, current);
+        match = true;
+        break;
+      }
+    }
+    if (!match) { connections.push(current) }
+  } else if (connections.length === config.maxActivePatterns) {
+    // remove last sent connection and replace with the new one
+    connections.push(current)
+    connections.shift()
+  }
+
+  for ([i, connection] of connections.entries()) {
+    tidal.stdin.write(`d${i + 1} \$ ${connection.pattern}\n`);
+  }
+
+  console.log(connections);
+
   return msg;
 }
 
@@ -51,7 +72,7 @@ function onMessageHandler (target, context, msg, self) {
       twitchClient.say(target, result);
       break;
       // username as argument (for multiuser debug/testing only)
-      // e.g. !u kuhn d1 ... to pretend to be user 'kuhn'
+      // e.g. `!u kuhn msg` to pretend to be user 'kuhn'
     case "!u":
       const tmsg = msg.split(' ');
       const user = tmsg.splice(1, 1);
