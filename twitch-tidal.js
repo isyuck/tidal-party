a33050169fd67a3ef705a4df38b5f2bae609f864
isaac <isaac@isaac.ac>
2021-04-16 15:42:33 +0100

    debug cmd !u to fake username

--- twitch-tidal.js
+++ twitch-tidal.js
@@ -23,8 +23,6 @@ tidal.stderr.on('data', (data) => {
   console.error(`stderr: ${data}`);
 });
 
-
-
 // ui.render();
 
 function updateUI() {
@@ -36,7 +34,7 @@ function updateUI() {
 function handleNewMessage(msg, username) {
     // updateUI()
   msg = msg.substr(msg.indexOf(" ") + 1)
-  console.log(username);
+  console.log(msg, username);
   tidal.stdin.write(msg + "\n");
   return msg;
 }
@@ -51,6 +49,13 @@ function onMessageHandler (target, context, msg, self) {
     case "!t":
       const result = handleNewMessage(msg, context.username);
       twitchClient.say(target, result);
+      break;
+      // username as argument (for multiuser debug/testing only)
+      // e.g. !u kuhn d1 ... to pretend to be user 'kuhn'
+    case "!u":
+      const tmsg = msg.split(' ');
+      const user = tmsg.splice(1, 1);
+      handleNewMessage(tmsg.join(' '), user.join(' '));
       break;
   }
 }
